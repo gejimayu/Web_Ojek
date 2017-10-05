31c246e9fbdfa9956fa962051511b4ed0c283c83
gejimayu <gianffh@gmail.com>
2017-10-05 20:09:10 +0700

    Add edit pref location table display

---
+++ edit_pref_loc.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style/header.css">
+	<link rel="stylesheet" type="text/css" href="style/edit_pref_loc.css">
+	<title>Order</title>
+</head>
+<body>
+	<div>
+		<p id="hi_username">Hi, <b>pikachu</b> !</p>
+		<h1 id="logo">
+			<span id="labelgreen">PR</span>-<span id="labelred">OJEK</span>
+		</h1>
+		<a id="logout" href="#">Logout</a>
+		<p id="extralogo">wush... wush... ngeeeeenggg...</p>
+	</div>
+
+	<table id="tableactivity">
+		<tr>
+			<td class="rest_activity">ORDER</td>
+			<td class="rest_activity">HISTORY</td>
+			<td id="current_activity">MY PROFILE</td>
+		</tr>
+	</table>
+
+	<p id="editprefloc">EDIT PREFERRED LOCATIONS</p>
+
+	<table id="tableprefloc">
+		<tr id="titlecolumn">
+			<td>No</td>
+			<td>Location</td>
+			<td>Actions</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td id="city">Saffron City</td>
+			<td id="editbutton"><img id="pencil" src="vstock/pencil.png"><img id="xmark" src="vstock/forbidden-mark.png"></td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td id="city">Vienna City</td>
+			<td id="editbutton"><img id="pencil" src="vstock/pencil.png"><img id="xmark" src="vstock/forbidden-mark.png"></td>
+		</tr>
+
+	</table>
+
+</body>
+</html>
