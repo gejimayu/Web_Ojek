76e12d3fd0e043188518698147c6aaaa42f483bb
stevannohero <13515082@std.stei.itb.ac.id>
2017-10-05 18:13:12 +0700

    change html to php showprofile

---
+++ editprofile.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Edit Profile </title>
+			<link rel="stylesheet" href="editprofilelayout.css">
+			<link href='https://fonts.googleapis.com/css?family=Fjalla+One' rel='stylesheet'>
+	</head>
+	<body>
+		<h1>EDIT PROFILE INFORMATION</h1>
+		<img src="vstock/blank_ava.png" alt="avatar">
+		<div class="edit_ava">
+			<h2>Update profile picture</h2>
+				<input id="avaBox" type="text" name="inputAva">
+				<input class="upload" id="browseButton" type="submit">
+		</div>
+		<div class="edit_profile">
+			<form>
+				<table>
+					<tr>
+						<td class="labelTabel">Your Name</td>
+						<td><input class="box" type="text" name="inputName" required></td>
+					</tr>
+					<tr>
+						<td class="labelTabel">Phone</td>
+						<td><input class="box" type="number" name="inputPhone" required></td>
+					</tr>
+					<tr>
+						<td class="labelTabel">Status Driver</td>
+						<td>
+							<label class="switch">
+								<input type="checkbox">
+								<span class="slider round"></span>
+							</label>
+						</td>
+					</tr>
+				</table>
+			</form>
+		</div>
+		<input id="backButton" type="submit" value="BACK">
+		<input id="saveButton" type="submit" value="SAVE">
+	</body>
+</html>
--- showprofile.php
+++ showprofile.php
@@ -8,13 +8,13 @@
 	</head>
 	<body>
 		<?php
+			include 'db.php';
 			$db = new Database();
-			$rows = $db -> select("SELECT * FROM user");
-
-			
-		?>
+			$rows = $db -> select("SELECT * FROM user WHERE id_user=4");
+			$rowsdriver = $db -> select("SELECT * FROM driver WHERE id_driver=4");
+		?>	
 		<div>
-			<p id="hi_username">Hi, <b>kursikuda</b> !</p>
+			<p id="hi_username">Hi, <b><?php echo $rows[0]['name'] ?></b>!</p>
 			<h1 id="logo">
 				<span id="labelgreen">PR</span>-<span id="labelred">OJEK</span>
 			</h1>
@@ -34,13 +34,21 @@
 				<img id ="pencil" src="vstock/pencil.png">
 			</div>
 			<div class="show_info">
-				<img id="user" src="vstock/blank_ava.png" alt="avatar"><br/>
-				<span>@kursikuda</span>
+				<img id="user" src=<?php echo $rows[0]['prof_pic'] ?>  alt="avatar"><br/>
+				<span>@<?php echo $rows[0]['username']?></span>
 				<div class="usersInfo">
-					Kuda Lumping <br/>
-					Driver | <label class="star"> ☆4.7 </label>(1728 votes) <br/>
-					✉ kudakudaanyuk@gajah.com <br/>
-					☏ 08123456789 <br/>
+					<?php echo $rows[0]['name']?> <br/>
+					<?php if ($rows[0]['driver_status']) {
+						echo 'Driver ';
+						echo '|';
+						echo '<label class="star"> ☆'.
+						$rowsdriver[0]['avgrating'].
+						'</label>(1728 votes) <br/>';
+					} else {
+						echo 'User';
+					}?>
+					✉ <?php echo $rows[0]['email']?> <br/>
+					☏ <?php echo $rows[0]['phone_number']?> <br/>
 				</div>
 			</div>
 			<div class="heading_loc">
