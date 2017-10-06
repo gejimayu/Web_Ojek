daa2a7349c9a868ec26811aa1c2e4893bbe7b863
stevannohero <13515082@std.stei.itb.ac.id>
2017-10-07 01:41:50 +0700

    fix front end of login and register

---
+++ login fix/login.php
@@ -0,0 +1,36 @@
+<?php include('validateLogin.php'); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Page</title>
+	<link rel="stylesheet" type="text/css" href="style/login.css">
+	<link href='https://fonts.googleapis.com/css?family=Fjalla+One' rel='stylesheet'>
+</head>
+<body>
+	<div class="content">
+		<div class="header">
+			<h1 id="title">── LOGIN ──</h1>
+		</div>
+		<?php if(!empty($error)) { ?>	
+			<div id="error"><?php echo $error; ?></div>
+		<?php } ?>
+		<form method="post" action="login.php" onsubmit="return validateForms()">
+			<table>
+				<tr>
+					<td><label id="inputLabel" for="username">Username</label></td>
+					<td><input id="box" placeholder="Insert Your Username" type="text" name="username" id="username"  /></td>
+				</tr>
+				<tr>
+					<td><label id="inputLabel" for="password">Password</label></td>
+					<td><input id="box" placeholder="Insert Your Password" type="password" name="password" id="password" /></td>
+				</tr>
+			</table>
+			<div class="bottom">
+				<a id="no_account" href="register.php">Don't have an account?</a>
+				<button type="submit" class="buttonGO" name="login">GO!</button>
+			</div>
+		</form>
+	</div>		
+</body>
+</html>
